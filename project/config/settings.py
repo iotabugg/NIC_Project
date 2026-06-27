96005d7fb19c0d4fb4ee33b22b0e6e135a63d5c0
iotabugg <rishabh.ucx@gmail.com>
2026-06-27 14:47:54 +0530

    fixed the session issue in users module, now django also saves the session of the user.

--- project/config/settings.py
+++ project/config/settings.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 from decouple import config
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+LOGIN_URL = '/login/'
 SECRET_KEY = config('SECRET_KEY', default='change-me-in-production')
 DEBUG = config('DEBUG', default=True, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
